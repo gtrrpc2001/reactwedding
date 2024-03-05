7eb3ab0fc9d69aa8d1712bb265d47bb635cf8a1e
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2024-03-05 13:32:27 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,13 @@ import { Layout } from './layout/layout';
 import { Main } from './main/main';
 
 function App() {
-<<<<<<< HEAD
-  return (      
-=======
   return (  
->>>>>>> f87cf1015c4d6baa25dbc64593ea94a7a1e10be4
       <Routes>
         <Route path='/' element={<Layout/>}>
           <Route index element={<Main/>}/>
         </Route>
         <Route path='*' element={<Navigate to={"/"} replace={true}/>}/>
-<<<<<<< HEAD
       </Routes>      
-=======
-      </Routes>
->>>>>>> f87cf1015c4d6baa25dbc64593ea94a7a1e10be4
   );
 }
 
