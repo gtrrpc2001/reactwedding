840ee3a78efeb88409b5a9d0a84bbd32ab372b16
gtrrpc2001 <cbdgjs3548@naver.com>
2024-03-06 09:55:24 +0900

    comw

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -5,11 +5,7 @@ type props={
 
 export const Main = ({width,height}:props) => {
     return (
-<<<<<<< HEAD
         <main style={{width:width,height:height}}>   
-=======
-        <main style={{width:width,height:height}}>  
->>>>>>> 108d870146b96bb4af3c125fe1d12888895257d7
         </main>
     );
 }
