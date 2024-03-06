01600c6159a4e9e40a5dc8a3c0a778a173efb850
gtrrpc2001 <cbdgjs3548@naver.com>
2024-03-06 15:23:27 +0900

    asd

--- src/gallery/gallery.tsx
+++ src/gallery/gallery.tsx
@@ -34,7 +34,7 @@ export const Gallery = ({width,height,borderRadius}:props) => {
             </h2>
           </div>
           <div className='gallery'>
-            <div style={{width:width * 0.7, height:height * 0.6}}>              
+            <div style={{width:width * 0.7, height:height * 0.6, objectFit:'cover'}}>              
               <Carousel
               showArrows={false}
               autoPlay={true}
