071a812910e910e811ee137788e5c8a6d737fec3
gtrrpc2001 <cbdgjs3548@naver.com>
2024-03-06 17:30:06 +0900

    aweqwe

--- src/congratulationGift/congratulationGift.tsx
+++ src/congratulationGift/congratulationGift.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Button } from '@mui/material'
+import { CreditCard } from "../creditcard/creditCard";
 
 type props={
     width:number
@@ -7,7 +9,6 @@ type props={
 }
 
 export const CongratulationGift = ({width,height}:props) => {
-    const [visible, setVisible] = useState(false);
 
     const handleCopy = (accountNumber:string) => {
         alert("계좌번호가 복사되었습니다");
@@ -17,33 +18,24 @@ export const CongratulationGift = ({width,height}:props) => {
         <div>
           <span>{receiver}</span>
           <CopyToClipboard text={accountNumber} onCopy={handleCopy}>
-            <button>{accountNumber}</button>
+            <Button>{accountNumber}</Button>
           </CopyToClipboard>
         </div>
-    );
+    );    
 
-    const noClipboardButton = (receiver:string) => (
-        <div>
-          <button onClick={() => setVisible(!visible)}>{receiver}</button>
+    return (
+        <div style={{width:width*0.8,height:height*0.8}}> 
+            <div style={{marginTop:20,height:height*0.4,textAlign:'center'}}>
+              <h2 style={{ color: '#295234', fontWeight: "bold", fontSize: 20 }}>신랑 신부에게 마음 전하기</h2>
+              {buildButton('신랑 이충헌', '카카오뱅크 3333-24-8680799')}
+              {buildButton('신부 이알레이나', '하나은행 702-910962-53707')}
+            </div>
+            <div style={{height:height*0.35}}>
+
+            </div>
+            <div style={{height:height*0.05}}>
+                <CreditCard />      
+            </div>
         </div>
     );
-
-    const getVisibleText = () => (
-        visible && (
-          <div>
-            <p>신랑 이충헌 : 카카오뱅크 3333-24-8680799</p>
-            <p>신부 이알레이나 : 하나은행 702-910962-53707</p>
-          </div>
-        )
-      );
-
-      return (
-        <div style={{ marginTop:20 ,width:width*0.8,height:height*0.8,textAlign:'center'}}>
-          <h2 style={{ color: '#295234', fontWeight: "bold", fontSize: 20 }}>신랑 신부에게 마음 전하기</h2>
-          {buildButton('신랑 이충헌', '카카오뱅크 3333-24-8680799')}
-          {buildButton('신부 이알레이나', '하나은행 702-910962-53707')}
-          {noClipboardButton('계자번호가 복사가 안될 경우 클릭')}
-          {getVisibleText()}
-        </div>
-      );
 }
---
+++ src/creditcard/creditCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './creditCard.css';
+
+export const CreditCard = () => {
+    return (
+        <div className="credit">
+          © 2024 이충헌, Built with React
+        </div>
+    );
+}
